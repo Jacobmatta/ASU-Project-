7ea88e3eba22292f9e0c1649531b6c78dc67c2a8
Jacob Matta <jpm86668@uga.edu>
2023-02-14 15:59:02 -0500

    Summary for Question 32
    Question 32 addresses HC-HIT-01

--- HC:HIT-01+02+03_Summary.R
+++ HC:HIT-01+02+03_Summary.R
@@ -19,8 +19,18 @@ summary(rawdata)
 #getting a count for our indicator PM #5 HC-HIT 01  
 Q32 = rawdata$Question_32
 
+#replacing NA values with *
+Q32 = Q32 %>% replace(is.na(.), "*")
+
 #getting summary of question 32
-Q32_summary = describe(Q32) 
+describe(Q32) 
+
+#making clean datatable with summary of question 32
+Q32_summary = data.frame (Value  = c("Missing", "Yes","No", "Not sure", "Do not know"),
+                         Frequency = c(49, 404, 17, 14, 4))
+Q32_summary
+
+
 
 
 
