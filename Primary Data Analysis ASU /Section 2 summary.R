26e66d49e912b38d73a5d2f06afb6c75f122bd11
Jacob Matta <jpm86668@uga.edu>
2023-03-05 18:09:43 -0500

    Further summarizing
    left off summarizing q3 and 4 which are continuous and need an approach to describe those variables

--- Primary Data Analysis ASU /Section 2 summary.R
+++ Primary Data Analysis ASU /Section 2 summary.R
@@ -8,6 +8,8 @@ library(readxl)
 library(writexl)
 library(data.table)
 library(stringi)
+library(Hmisc)
+library(ggplot2)
 
 #importing the file with surveys up until Feb 28th 
 #you will have to chane the file location 
@@ -45,7 +47,7 @@ Feb28$Age <- ifelse(Feb28$Age == 1, "18-24 years old",
                                                                     ifelse(Feb28$Age == 7, "75 years or older", NA)))))))
 ##Race_Ethnicity
 Feb28$Race_Ethnicity <- ifelse(Feb28$Race_Ethnicity == 1, "African American", 
-                                            ifelse(Feb28$Race_Ethnicity == 1, "Caucasian",
+                                            ifelse(Feb28$Race_Ethnicity == 2, "Caucasian",
                                                    ifelse(Feb28$Race_Ethnicity == 3, "Latino or Hispanic",
                                                           ifelse(Feb28$Race_Ethnicity == 4, "Asian",
                                                                  ifelse(Feb28$Race_Ethnicity == 5, "Native American",
@@ -179,28 +181,159 @@ Feb28$Question_21 <- ifelse(Feb28$Question_21 == 1, "Yes",
                             ifelse(Feb28$Question_21 == 2, "No",
                                    ifelse(Feb28$Question_21 == 3, "Not sure",
                                           ifelse(Feb28$Question_21 == 4, "Do not know", NA))))
+###########################################################Summary for race, age and insurance
+#creating values for race, age and insurance questions
+race = Feb28$Race_Ethnicity
+age = Feb28$Age
+insurance = Feb28$Health_Insurance
+
+#replacing NA values with "missing" 
+race = race %>% replace(is.na(.), "missing")
+age = age %>% replace(is.na(.), "missing")
+insurance = insurance %>% replace(is.na(.), "missing")
+
+#creating frequency table for race
+describe(race) #offers frequency and proportion of response 
+table(race)
+race_tib = tibble(race) #used to make a tibble out of the count 
+
+#making a bar graph of race of participants 
+race_barplot = ggplot(race_tib , aes(x=race, fill=race)) + #have to use the tibble of the count of race here 
+  geom_bar( ) +
+  scale_fill_hue(c = 40) +
+  theme(legend.position="none") 
+race_barplot + ggtitle("Count of Participant by Race")
+
+#creating a frequency table for age 
+describe(age) #offers frequency and proportion of response 
+table(age)
+age_tib = tibble(age) #used to make a tibble out of the count 
+
+#making a bar graph of age of participants 
+age_barplot = ggplot(age_tib , aes(x=age, fill=age)) + #have to use the tibble of the count of race here 
+  geom_bar( ) +
+  scale_fill_hue(c = 40) +
+  theme(legend.position="right", axis.title.x=element_blank(),axis.text.x=element_blank(),
+        axis.ticks.x=element_blank()) 
+age_barplot + ggtitle("Count of Participant by age") 
+
+#creating frequency table for insurance 
+describe(insurance) #offers frequency and proportion of response 
+table(insurance)
+insurance_tib = tibble(insurance) #used to make a tibble out of the count 
+
+#making a bar graph of insurance of participants 
+insurance_barplot = ggplot(insurance_tib , aes(x=insurance, fill=insurance)) + #have to use the tibble of the count of race here 
+  geom_bar( ) +
+  scale_fill_hue(c = 40) +
+  theme(legend.position="right", axis.title.x=element_blank(),axis.text.x=element_blank(),
+        axis.ticks.x=element_blank()) 
+insurance_barplot + ggtitle("Count of Participant by insurance") 
+
+######################################################### question 1 by race, age and insurance 
 
 #summing question 1 by age
-Q1_by_age = table(Feb28$Age, Feb28$Question_1)
+##creating values for question 1 
+Q1 = Feb28$Question_1
 
-Q1_by_age_tibble = as_tibble(Q1_by_age, rownames= "Missing", )
+#replacing NA values with "missing" for both age and Q1 
+Q1 = Q1 %>% replace(is.na(.), "missing")
 
+#creating frequency table for insurance 
+describe(Q1)
+table(Q1)
+Q1_tib = tibble(Q1)
 
+#making a bar graph of Q1 of participants 
+Q1_barplot = ggplot(Q1_tib , aes(x=Q1, fill=Q1)) + #have to use the tibble of the count of race here 
+  geom_bar( ) +
+  scale_fill_hue(c = 40) +
+  theme(legend.position="right", axis.title.x=element_blank(),axis.text.x=element_blank(),
+        axis.ticks.x=element_blank()) 
+#adding a title and subtitle
+Q1_barplot = Q1_barplot + labs(title = "Count of Participant by Q1", 
+                                subtitle = "Has a healthcare provider or a home test ever informed you or your family members that you (or your children or other family members) had COVID-19?") 
 
-#trying to summarize with the summarise funtion
-Feb28_arranged = Feb28 %>%
-  arrange(Age, Race_Ethnicity, Health_Insurance) 
+#editing the subtitle font size
+Q1_barplot = Q1_barplot + theme(plot.title = element_text(color = "black"),
+                                plot.subtitle = element_text(color = "black", size = 6))
+Q1_barplot
 
-df <- data.frame(
-  gender = c("Male", "Female", "Male", "Male", "Female", "Female"),
-  age = c(25, 30, 35, 25, 30, 35)
-)
+# Use table() function to get frequency table for Q1 vs Race
+Q1_race_freq = table(Q1, race)
+Q1_race_freq
 
-# Use table() function to get frequency table
-freq_table <- table(df$gender, df$age)
+# Use table() function to get frequency table for Q1 vs Age
+Q1_age_freq = table(Q1, age)
+Q1_age_freq
 
-# Convert the frequency table to a tibble
-freq_tibble <- as_tibble(freq_table, rownames = "gender", colnames = "age")
+# Use table() function to get frequency table for Q1 vs insurance
+Q1_insurance_freq = table(Q1, insurance)
+Q1_insurance_freq
 
-# View the resulting tibble
-freq_tibble
+#########################################################question 2 by race, age and insurance 
+##creating values for question 2 
+Q2 = Feb28$Question_2
+
+#replacing NA values with "missing" for both age and Q2 
+Q2 = Q2 %>% replace(is.na(.), "*")
+
+#creating frequency table for Q2 
+describe(Q2)
+table(Q2)
+Q2_tib = tibble(Q2)
+
+#making a bar graph of Q2 of participants 
+Q2_barplot = ggplot(Q2_tib , aes(x=Q2, fill=Q2)) + #have to use the tibble of the count of race here 
+  geom_bar( ) +
+  scale_fill_hue(c = 40) +
+  theme(legend.position="right", axis.title.x=element_blank(),axis.text.x=element_blank(),
+        axis.ticks.x=element_blank()) 
+#adding a title and subtitle
+Q2_barplot = Q2_barplot + labs(title = "Count of Participant by Q2", 
+                               subtitle = "How many members of your household tested positive for COVID-19") 
+
+#editing the subtitle font size
+Q2_barplot = Q2_barplot + theme(plot.title = element_text(color = "black"),
+                                plot.subtitle = element_text(color = "black", size = 6))
+Q2_barplot
+
+# Use table() function to get frequency table for Q2 vs Race
+Q2_race_freq = table(Q2, race)
+Q2_race_freq
+
+# Use table() function to get frequency table for Q2 vs Age
+Q2_age_freq = table(Q2, age)
+Q2_age_freq
+
+# Use table() function to get frequency table for Q2 vs insurance
+Q2_insurance_freq = table(Q2, insurance)
+Q2_insurance_freq
+#########################################################Summary for question 3 and 4
+##creating values for question 3
+Q3 = Feb28$Question_3
+
+#replacing NA values with "missing" for both age and Q3 
+Q3 = Q3 %>% replace(is.na(.), "*")
+
+#creating frequency table for Q3 
+#Q3 is an open ended question
+#note that Q3 is continuous and need to treat as a continuous variable 
+describe(Q3)
+table(Q3)
+Q3_tib = tibble(Q3)
+
+#making a bar graph of Q3 of participants 
+Q3_barplot = ggplot(Q3_tib , aes(x=Q3, fill=Q3)) + #have to use the tibble of the count of race here 
+  geom_bar( ) +
+  scale_fill_hue(c = 40) +
+  theme(legend.position="right", axis.title.x=element_blank(),axis.text.x=element_blank(),
+        axis.ticks.x=element_blank()) 
+#adding a title and subtitle
+Q3_barplot = Q3_barplot + labs(title = "Count of Participant by Q3", 
+                               subtitle = "Age of family member one who had COVID-19") 
+
+#editing the subtitle font size
+Q3_barplot = Q3_barplot + theme(plot.title = element_text(color = "black"),
+                                plot.subtitle = element_text(color = "black", size = 6))
+Q3_barplot
